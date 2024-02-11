ba276e77892695edaee842b229395f9f5f4871cd
haqimregov <haqim.syahril@regovtech.com>
2024-02-11 17:37:45 +0800

    Editted file structure - added utils.py

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,5 @@
 from zipfile import ZipFile
+from utils import zip_file, read_object_from_s3
 import os
 import json 
 import logging 
@@ -8,30 +9,9 @@ import dateutil.tz
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def zip_file(source_file,output_file):
-
-    try:
-        with ZipFile(output_file,'w') as zip:
-            zip.write(source_file, arcname=os.path.basename(source_file))
-
-        logger.info(f"File {os.path.basename(source_file)} has been zipped.")
-        return True
-    
-    except Exception as e:
-        logger.error(f"An error occurred while zipping: {str(e)}")
-        return False
-
-def read_object_from_s3(s3,bucket_name, object_key):
-    try:
-        response = s3.get_object(Bucket=bucket_name, Key=object_key)
-        return response['Body'].read()
-    except Exception as e:
-        logger.error(f"An error occurred while reading from S3: {str(e)}")
-        return None
 
 def event_handler(s3,event):
 
-    
 
     for record in event['Records']:
         # Get bucket and key from the event
---
+++ src/utils.py
@@ -0,0 +1,30 @@
+from zipfile import ZipFile
+import os
+import json 
+import logging 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# zip file function
+def zip_file(source_file,output_file):
+
+    try:
+        with ZipFile(output_file,'w') as zip:
+            zip.write(source_file, arcname=os.path.basename(source_file))
+
+        logger.info(f"File {os.path.basename(source_file)} has been zipped.")
+        return True
+    
+    except Exception as e:
+        logger.error(f"An error occurred while zipping: {str(e)}")
+        return False
+
+# read object from s3 function
+def read_object_from_s3(s3,bucket_name, object_key):
+    try:
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        return response['Body'].read()
+    except Exception as e:
+        logger.error(f"An error occurred while reading from S3: {str(e)}")
+        return None
